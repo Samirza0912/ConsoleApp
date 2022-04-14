839eb24e968f0eb31e5eb8e69ef17ff6e93ae60d
Samirza0912 <samirza@code.edu.az>
2022-04-14 14:41:43 +0400

    feat:i add academy app

--- AcademyApp/Controllers/StudentController.cs
+++ AcademyApp/Controllers/StudentController.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Business.Services;
+using Entities.Models;
+using Utilities.Helper;
 
 namespace AcademyApp.Controllers
 {
     class StudentController
     {
+        GroupService groupService = new GroupService();
+        StudentService studentService { get; set; }
+        
+        public StudentController()
+        {
+            studentService = new StudentService();
+        }
+        public void CreateStudent()
+        {
+        EnterName:
+            Extention.Print(ConsoleColor.Green, $"Student Name");
+            string name = Console.ReadLine();
+            bool isSize = false;
+            if (isSize)
+            {
+                Student student = new Student
+                {
+                    Name = name,
+                    
+                };
 
-       
+                studentService.Create(student);
+                Extention.Print(ConsoleColor.Green, $"{student.Name} created");
+            }
+            else
+            {
+                Extention.Print(ConsoleColor.Red, "Enter the correct one");
+                goto EnterName;
+            }
+        }
+        public void GetAllStudents()
+        {
+            foreach (var item in studentService.GetAll())
+            {
+                Extention.Print(ConsoleColor.Yellow, $"{item.Name}");
+            }
+        }
+        public void RemoveStudent()
+        {
+            int id = int.Parse(Console.ReadLine());
+            Extention.Print(ConsoleColor.Green, $"{studentService.Delete(id).Name}");
+        }
     }
 }
